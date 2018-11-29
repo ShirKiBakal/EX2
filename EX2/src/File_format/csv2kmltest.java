bfc75a050e3c128bd1e474642a4de83940217b5c
ShirKiBakal <shiranbakal@gmail.com>
2018-11-29 16:43:12 +0200

    fix typo in csk2kml

--- EX2/src/File_format/csv2kmltest.java
+++ EX2/src/File_format/csv2kmltest.java
@@ -6,9 +6,9 @@ public class csv2kmltest {
 
 	public static void main(String[] args) {
 		String source_file ="C:\\Users\\shiran.b\\Downloads\\Ex2\\Ex2\\data\\123.csv";
-		String ourput_file ="C:\\Users\\shiran.b\\Downloads\\Ex2\\Ex2\\data\\123.kml";
+		String output_file ="C:\\Users\\shiran.b\\Downloads\\Ex2\\Ex2\\data\\123.kml";
 		try{
-		Csv2Kml c2k = new Csv2Kml(source_file,ourput_file);
+		Csv2Kml c2k = new Csv2Kml(source_file,output_file);
 		c2k.run();
 		String[] headres = c2k.getHeaders();
 		System.out.println("headers are:"+Arrays.deepToString(headres));
