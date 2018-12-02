054cec32c24f600a933b827502fdadc89636a58b
ShirKiBakal <shiranbakal@gmail.com>
2018-12-02 21:15:04 +0200

    finish documenting and making javadoc

--- EX2/src/Coords/MyCoords.java
+++ EX2/src/Coords/MyCoords.java
@@ -2,10 +2,20 @@ package Coords;
 
 import Geom.Point3D;
 
+/**
+ * this class calculate different function of 3D Point.
+ * @author Shiran.b
+ *
+ */
 public class MyCoords implements coords_converter{
 
 	private final int earth_radius = 6371000;
-
+	
+	/**
+	 *  this function computes a new point which is the gps point transformed by a 3D vector (in meters)
+	 *  @param Point3D gps, Point3D local_vector
+	 *  @return Point3D answer
+	 */
 	public Point3D add(Point3D gps, Point3D local_vector_in_meter) {
 		double answerX=local_vector_in_meter.toDegrees(local_vector_in_meter.x());
 		answerX+=local_vector_in_meter.fromRadian(answerX);
@@ -24,7 +34,11 @@ public class MyCoords implements coords_converter{
 	
 	}
 
-	@Override
+	/**
+	 *  this function computes the 3D distance (in meters) between the two gps like points 
+	 * @param Point3D gps0, Point3D gps1
+	 * @return double dist
+	 */
 	public double distance3d(Point3D gps0, Point3D gps1) {
 		double dist = 0;
 		double dif_lat_rad = Math.toRadians(gps0.x()-gps1.x());
@@ -37,7 +51,11 @@ public class MyCoords implements coords_converter{
 		return dist;
 	}
 
-	@Override
+	/**
+	 * this function computes the 3D vector (in meters) between two gps like points 
+	 * @param Point3D gps0, Point3D gps1
+	 * return Point3D ans 
+	 */
 	public Point3D vector3D(Point3D gps0, Point3D gps1) {
 		double x = Math.abs(gps0.x()-gps1.x());
 		double y = Math.abs(gps0.y()-gps1.y());
@@ -46,7 +64,11 @@ public class MyCoords implements coords_converter{
 		return ans;
 	}
 
-	@Override
+	/** this function computes the polar representation of the 3D vector be gps0-->gps1 
+	 * Note: this method should return an azimuth (aka yaw), elevation (pitch), and distance
+	 * @param Point3D gps0, Point3D gps1
+	 * @return arr (two points)
+	 */
 	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
 		double[] arr = new double[3];
 		arr[0] = gps0.north_angle(gps1);
@@ -55,7 +77,11 @@ public class MyCoords implements coords_converter{
 		return arr;
 	}
 
-	@Override
+	/**
+	 * return true iff this point is a valid lat, lon , lat coordinate: [-180,+180],[-90,+90],[-450, +inf]
+	 * @param Point3D p
+	 * @return boolean flag
+	 */
 	public boolean isValid_GPS_Point(Point3D p) {
 		double x=p.x();
 		double y=p.y();
